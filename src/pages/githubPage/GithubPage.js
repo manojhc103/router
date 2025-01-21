aae2668837dd4ea51e21ac1a6ff2a2f16a350080
Manoj <mhalsar15@gmail.com>
2025-01-21 15:21:34 +0530

    update

--- src/pages/githubPage/GithubPage.js
+++ src/pages/githubPage/GithubPage.js
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function GithubPage() {
+
+const[githubData ,setGithubData] = useState('')
+// console.log(githubData);
+
+
+useEffect (()=>{
+     setApiData()
+},[])
+
+  const setApiData = async()=>{
+    const response = await  fetch('https://api.github.com/users/hiteshchoudhary')
+
+    if(response.ok){
+    const data = await response.json()
+    setGithubData(data)
+    }else{
+      alert("error")
+      
+    }
+  }
+
   return (
-    <div>GithubPage</div>
+    <div className='text-center'>
+      {/* <h1>github page</h1> */}
+      <h2>Name: {githubData.name}</h2>
+      <img  className='w-25' src={githubData.avatar_url} alt=""/>
+
+
+    </div>
   )
 }
 
